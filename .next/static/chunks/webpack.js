ec674a01383848993bc19d53183ac048ac56c01f
Lucas Padilha <lucas_padilha@icloud.com>
2022-12-19 18:09:30 +0000

    Changed favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d19674b0fd312dfe"; }
+/******/ 		__webpack_require__.h = function() { return "97dce718f3131e22"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
